ee2da3f18f722ccb5102d1ff0251c43809e28919
reravn <reravennn@gmail.com>
2026-02-07 01:07:55 +0300

    m2

--- fs/proc/internal.h
+++ fs/proc/internal.h
@@ -122,16 +122,7 @@ static inline struct pid *proc_pid(const struct inode *inode)
 
 static inline struct task_struct *get_proc_task(const struct inode *inode)
 {
-	
-    struct task_struct * p = get_pid_task(proc_pid(inode), PIDTYPE_PID);
-    char tcomm[64];
-    if (p->flags & PF_WQ_WORKER)
-            wq_worker_comm(tcomm, sizeof(tcomm), p);
-    else
-            __get_task_comm(tcomm, sizeof(tcomm), p);
-    if (strstr(tcomm, "frida") || strstr(tcomm, "florida") || strstr(tcomm, "hluda") || strstr(tcomm, "gmain") || strstr(tcomm, "gum-js") || strstr(tcomm, "linjector") ||  strstr(tcomm, "gdbus"))
-            return NULL;
-    return p;
+	return get_pid_task(proc_pid(inode), PIDTYPE_PID);
 }
 
 void task_dump_owner(struct task_struct *task, umode_t mode,
